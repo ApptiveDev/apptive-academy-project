d8002f0c5f4c62655e8740a8757eed67c7c96083
cla6shade <cla6shade@gmail.com>
2024-05-14 21:24:29 +0900

    chore: 주요 경로에 대한 alias 등록
    리소스에 대한 상대경로 대신 @로 시작하는 alias 사용 가능.
    
    ex) ./src/css/index.css -> @css/index.css, ./src/assets -> @assets

--- template/frontend/apptive/vite.config.js
+++ template/frontend/apptive/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@css", replacement: "/src/css" },
+      { find: "@", replacement: "/src" },
+    ],
+  },
 })
