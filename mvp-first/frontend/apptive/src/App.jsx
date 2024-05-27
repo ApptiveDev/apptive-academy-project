81a89193412e364590a8d80ab5d533cbfa2e4b81
uicheol <suc2150@hanmail.net>
2024-05-27 11:52:44 +0900

    feat: add Input component

--- mvp-first/frontend/apptive/src/App.jsx
+++ mvp-first/frontend/apptive/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./css/App.css";
+import Input from "./components/input/Input";
 
 function App() {
   return (
@@ -12,7 +13,10 @@ function App() {
             <div className="form__details">
               이름과 전화번호를 통해 AS 정보를 불러올 수 있어요
             </div>
-            <div className="form__input"></div>
+            <div className="form__input">
+              <Input placeholder={"이름"} />
+              <Input placeholder={"전화번호"} />
+            </div>
           </div>
         </div>
       </div>
---
+++ mvp-first/frontend/apptive/src/components/input/Input.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Input.css";
+
+const Input = ({ placeholder }) => {
+  return <input className="input" placeholder={placeholder} />;
+};
+
+export default Input;
