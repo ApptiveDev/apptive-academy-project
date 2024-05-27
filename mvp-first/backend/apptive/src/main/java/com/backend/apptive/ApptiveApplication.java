7d8d064b66360ff731474a7a9b97b6598a5c53f2
myeolinmalchi <rkd2274@pusan.ac.kr>
2024-05-28 02:22:25 +0900

    feat: mvp-first 미션 폴더 추가

---
+++ mvp-first/backend/apptive/src/main/java/com/backend/apptive/ApptiveApplication.java
@@ -0,0 +1,13 @@
+package com.backend.apptive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptiveApplication.class, args);
+	}
+
+}
