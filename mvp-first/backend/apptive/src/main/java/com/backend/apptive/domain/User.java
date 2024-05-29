eb53d4f8f87707302c23a6931f4e09ae9462508d
wvssm <neoramiregi@gmail.com>
2024-05-29 11:06:15 +0900

    feat(user): add User entity

---
+++ mvp-first/backend/apptive/src/main/java/com/backend/apptive/domain/User.java
@@ -0,0 +1,34 @@
+package com.backend.apptive.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "phoneNumber", unique = true, nullable = false)
+    private String phoneNumber;
+
+    // new ArrayList 해주기 ** 프록시 문제 관련
+    // @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    // private List<As> asList = new ArrayList<>();
+
+    @Builder
+    public User(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+}
