0e2eee82cb87d081e44080c5b82ca5226c43bc54
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-05-31 13:34:12 +0900

    first commit

--- mvp-first/backend/apptive/src/main/java/com/backend/apptive/ApptiveApplication.java
+++ mvp-first/backend/apptive/src/main/java/com/backend/apptive/ApptiveApplication.java
@@ -9,5 +9,4 @@ public class ApptiveApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApptiveApplication.class, args);
 	}
-		SpringApplication.run(ApptiveApplication.class, args);
 }
